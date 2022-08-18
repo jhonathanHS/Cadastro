8b63a4ff470a746167e35fef12ba8d0fce014a7a
jhonathanHS <74199754+jhonathanHS@users.noreply.github.com>
2022-08-17 23:56:17 -0300

    Change the views, strings and the "MainActivity" class

--- app/src/main/java/com/example/cadastro/MainActivity.java
+++ app/src/main/java/com/example/cadastro/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.cadastro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.cadastro.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding activityMainBinding;
+    private Formulario formulario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+
+        activityMainBinding.salvarBt.setOnClickListener(view -> {
+            formulario = new Formulario(
+                    activityMainBinding.nomeEt.getText().toString(),
+                    activityMainBinding.telefoneEt.getText().toString(),
+                    activityMainBinding.emailEt.getText().toString(),
+                    activityMainBinding.femininoRb.isChecked() ? getString(R.string.feminino) : getString(R.string.masculino),
+                    activityMainBinding.cidadeEt.getText().toString(),
+                    activityMainBinding.unidadeFederalSp.getSelectedItem().toString()
+            );
+            Toast.makeText(this, formulario.toString(), Toast.LENGTH_SHORT).show();
+        });
+
+        activityMainBinding.limparBt.setOnClickListener(view -> {
+            activityMainBinding.nomeEt.setText("");
+            activityMainBinding.telefoneEt.setText("");
+            activityMainBinding.emailEt.setText("");
+            activityMainBinding.femininoRb.setChecked(true);
+            activityMainBinding.cidadeEt.setText("");
+            activityMainBinding.unidadeFederalSp.setSelection(0);
+
+            formulario = null;
+
+        });
+
     }
 }
